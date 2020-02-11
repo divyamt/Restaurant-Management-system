0a13b72355c4c9dacf4ea31ee8f006496a4c6a88
Divyam Tyagi <iamdivyamtyagi@gmail.com>
2020-02-12 00:24:56 +0530

    Add files via upload

---
+++ class User:.py
@@ -0,0 +1,5 @@
+line = [10, 11, 12]
+while line <= 12:
+	print (line)
+	line += (1)
+	break
---
+++ resturent management.py
@@ -0,0 +1,13 @@
+from tkinter import*
+import random
+import time; 
+import datetime 
+import tkinter.messagebox
+
+root = Tk()
+root.geometry("1350x750+0+0")
+root.title("Restaurent Management System")
+root.configure(background = 'Cadet Blue')
+
+
+root.mainloop()
